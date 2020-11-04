504a64021723b6a48307b10f1d1ef680cbbab238
Matt Braddock <matt.braddock@gmail.com>
2020-11-04 13:10:39 -0500

    fixed most bugs, now beta

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Load modules
 const Discord = require('discord.js');
+const Twitter = require('twitter');
 const fetch = require('node-fetch');
 const oauthSignature = require('oauth-signature');
 const randomstring = require('randomstring');
@@ -9,9 +10,15 @@ const path = require('path');
 // Load configuration
 const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
 
-// Set up Discord
+// Set up Discord & Twitter
 const discord = new Discord.Client();
 discord.login(config.discord.token);
+const twitter = new Twitter({
+    "consumer_key": config.twitter.consumer_key,
+    "consumer_secret": config.twitter.consumer_secret,
+    "access_token_key": config.twitter.access_token_key,
+    "access_token_secret": config.twitter.access_token_secret
+});
 
 // Global variables
 let servers;
@@ -20,7 +27,7 @@ let assignments = {};
 const canvasInit = {
     "method": "GET",
     "headers": {
-        "Content Type": "application/json+canvas-string-ids",
+        "Content-Type": "application/json+canvas-string-ids",
         "Accept": "application/json+canvas-string-ids",
         "Authorization": "Bearer " + config.canvas.token
     }
@@ -28,49 +35,39 @@ const canvasInit = {
 const twitterGetInit = {
     "method": "GET",
     "headers": {
-        "content Type": "application/json",
+        "content-Type": "application/json",
         "Authorization": "Bearer " + config.twitter.bearer_token
     }
 }
-const twitterPostInit = params => ({
-    "method": "POST",
-    "headers": {
-        "oauth_consumer_key": config.twitter.consumer_key,
-        "oauth_nonce": randomstring(),
-        "oauth_signature": oauthSignature.generate('POST', tweetUrl, params, config.twitter.consumer_secret, config.twitter.access_token_secret),
-        "oauth_signature_method": "HMAC-SHA1",
-        "oauth_timestamp": Date.now().toString(),
-        "oauth_token": config.twitter.access_token_key,
-        "oauth_version": "1.0"
-    }
-});
-const tweetUrl = "https://api.twitter.com/1.1/statuses/update.json";
 
 // Sending a status/reply to Twitter
-const sendTwitter = async (status, replyTo = null) => {
-    let params = 'status=' + encodeURIComponent(status);
-    params += replyTo === null ? '' : '&in_reply_to_status_id=' + replyTo;
-    const tweetResponse = await fetch(tweetUrl + '?' + params, twitterPostInit(params))
+const sendTwitter = async (statusToTweet, replyTo = null) => {
+    const params = replyTo === null ? {
+        status: statusToTweet
+    } : {
+        status: statusToTweet,
+        in_reply_to_status_id: replyTo
+    };
+    await twitter.post('statuses/update', params)
         .catch(err => console.error(err));
 }
 
 // Sending a message to Discord
-const sendDiscord = async (embed, course) => {
-    // Get all servers watching the course
-    const receivingServers = servers.filter(s => s.course.id === course);
-    // Send embed
-    receivingServers.forEach(s => {
-        const thisChannel = await client.channels.fetch(s.channel);
-        await thisChannel.send(embed);
-    });
+const sendDiscord = async (embed, courseOrChannel, channel = false) => {
+    if (channel) await courseOrChannel.send(embed);
+    else {
+        // Get all servers watching the course
+        const receivingServers = servers.filter(s => s.course === courseOrChannel);
+        // Send embed
+        for (let i = 0; i < receivingServers.length; i++){
+            const thisChannel = await discord.channels.fetch(receivingServers[i].channel);
+            await thisChannel.send(embed);
+        }
+    }
 }
 
 // Convert HTML message to plain text
-const extract = content => {
-    let span = document.createElement('span');
-    span.innerHTML = content;
-    return span.textContent;
-}
+const extract = content => content.replace(/<[^>]+>/g, '');
 
 // Update servers
 const serverUpdate = () => {
@@ -84,7 +81,7 @@ const serverUpdate = () => {
 }
 
 // Function to get recent announcement
-const getAnnouncement = courseId => {
+const getAnnouncement = async courseId => {
     // Query Canvas for announcements
     const announcementsResponse = await fetch(config.canvas.domain + 'api/v1/announcements?context_codes[]=course_' + courseId, canvasInit);
     const announcementsArray = await announcementsResponse.json();
@@ -102,7 +99,7 @@ const getAnnouncement = courseId => {
 }
 
 // Function to get recent assignments
-const getAssignments = (courseId, groupId) => {
+const getAssignments = async (courseId, groupId) => {
     // Query Canvas for assignments
     const assignmentResponse = await fetch(config.canvas.domain + 'api/v1/courses/' + courseId + '/assignment_groups/' + groupId + '/assignments?bucket=upcoming&order_by=due_at', canvasInit);
     const assignmentArray = await assignmentResponse.json();
@@ -111,8 +108,10 @@ const getAssignments = (courseId, groupId) => {
     for (let i = 0; i < assignmentArray.length; i++) {
         // Stop once we find an assignment that was due before now
         if (new Date(assignmentArray[i].due_at) - now <= 0) break;
-        // If the assignment isn't unlocked, or we already have it, skip it
-        if (new Date(assignmentArray[i].unlock_at) - now > 0 || assignments[courseId].homework.find(hw => hw.id === assignmentArray[i].id) !== undefined) continue;
+        // If the assignment isn't unlocked, skip it
+        if (new Date(assignmentArray[i].unlock_at) - now > 0) continue;
+        // If this assignment isn't new, skip it
+        if (assignments[courseId].homework.find(hw => hw.id === assignmentArray[i].id !== undefined) || assignments[courseId].tests.find(t => t.id === assignmentArray[i].id !== undefined)) continue;
         const nextAssignment = {
             "id": assignmentArray[i].id,
             "title": assignmentArray[i].name,
@@ -126,8 +125,8 @@ const getAssignments = (courseId, groupId) => {
 }
 
 // To run once Discord has connected
-discord.once('ready', () => {
-    console.log('Canvas bot is online!');
+discord.once('ready', async () => {
+    console.log('Canvas bot is online @ ' + new Date(Date.now()));
     // Set Discord status
     discord.user.setPresence({
         "activity": {
@@ -151,27 +150,29 @@ discord.once('ready', () => {
 
     // Set global variables
     servers = JSON.parse(serverContents).servers;
-    config.canvas.courses.forEach(course => {
-        const recentAnnouncement = getAnnouncement(course.id);
-        announcements[course.id] = [recentAnnouncement];
-        const recentHomework = getAssignments(course.id, course.homework);
-        const recentTests = getAssignments(course.id, course.tests);
+    for (let i = 0; i < config.canvas.courses.length; i++) {
+        let course = config.canvas.courses[i];
+        announcements[course.id] = [];
+        const recentAnnouncement = await getAnnouncement(course.id);
+        announcements[course.id].push(recentAnnouncement);
         assignments[course.id] = {
-            "link": config.canvas.domain + "courses/" + course.Id + "/assignments",
-            "homework": recentHomework,
-            "tests": recentTests
-        };
-    });
+            "link": config.canvas.domain + "courses/" + course.id + "/assignments",
+            "homework": [],
+            "tests": []
+        }
+        assignments[course.id].homework = await getAssignments(course.id, course.homework);
+        assignments[course.id].tests = await getAssignments(course.id, course.tests);
+    }
 });
 
 // Listening for messages on Discord
 discord.on('message', async message => {
     // User must mention the bot
-    if (message.mentions.users.has(client.user.id)) {
+    if (message.mentions.users.has(discord.user.id)) {
         // Help message
         // Requires user to include the word "help"
         if (message.content.search(/help/i) > -1) {
-            message.reply('Need help? https://mattbraddock.com/canvas-bot/help');
+            message.reply('Need help? <https://mattbraddock.com/canvas-bot/help>');
             return;
         }
 
@@ -238,11 +239,11 @@ discord.on('message', async message => {
                 .addField('Posted:', thisAnnouncement.date)
                 .addField('Announcement:', announcementMessage)
                 .setTimestamp();
-            sendDiscord(announcementEmbed, course.id);
+            await sendDiscord(announcementEmbed, message.channel, true);
             return;
         } else if (message.content.search(/(homework|hw)/i) > -1) {
             const thisHomework = assignments[course.id].homework;
-            let homeworkMessage = '';
+            let homeworkMessage = thisHomework.length > 0 ? '' : 'No Upcoming Homework';
             thisHomework.forEach((h, i) => {
                 homeworkMessage += i === 0 ? '' : '\n';
                 homeworkMessage += h.title + ' (Due Date: ' + h.due_date + ')';
@@ -254,11 +255,11 @@ discord.on('message', async message => {
                 .setAuthor("Mr. Braddock's Canvas Bot")
                 .addField('Assignments:', homeworkMessage)
                 .setTimestamp();
-            sendDiscord(homeworkEmbed, course.id);
+            await sendDiscord(homeworkEmbed, message.channel, true);
             return;
         } else if (message.content.search(/(test|exam|quiz|tests|exams|quizzes)/i) > -1 ) {
             const thisTests = assignments[course.id].tests;
-            let testMessage = '';
+            let testMessage = thisTests.length > 0 ? '' : 'No Upcoming Tests';
             thisTests.forEach((t, i) => {
                 testMessage += i === 0 ? '' : '\n';
                 testMessage += t.title + ' (Date: ' + t.due_date + ')';
@@ -270,7 +271,7 @@ discord.on('message', async message => {
                 .setAuthor("Mr. Braddock's Canvas Bot")
                 .addField('Assessments:', testMessage)
                 .setTimestamp();
-            sendDiscord(testEmbed, course.id);
+            await sendDiscord(testEmbed, message.channel, true);
             return;
         } else {
             message.reply("I could not recognize what you're looking for. Available options are: announcement, homework, tests");
@@ -292,11 +293,11 @@ discord.setInterval(async () => {
         const thisCourse = config.canvas.courses[i];
 
         // Check for new announcements
-        const newAnnouncement = getAnnouncement(thisCourse.id);
-        if (newAnnouncement !== {}) {
+        const newAnnouncement = await getAnnouncement(thisCourse.id);
+        if (Object.keys(newAnnouncement).length !== 0 && newAnnouncement.constructor === Object) {
             // Tweet announcement
             const announcementStatus = '[' + thisCourse.name + '] ' + newAnnouncement.title + ' ' + newAnnouncement.link;
-            sendTwitter(announcementStatus);
+            await sendTwitter(announcementStatus);
             // Post announcement in Discord
             const announcementMessage = newAnnouncement.message.length >= 1024 ? newAnnouncement.message.substring(0, 1020) + '...' : newAnnouncement.message;
             const announcementEmbed = new Discord.MessageEmbed()
@@ -307,15 +308,16 @@ discord.setInterval(async () => {
                 .addField('Posted:', newAnnouncement.date)
                 .addField('Announcement:', announcementMessage)
                 .setTimestamp();
-            sendDiscord(announcementEmbed, course.id);
+            await sendDiscord(announcementEmbed, course.id);
         }
 
         // Check for new homework
-        const newHomework = getAssignments(thisCourse.id, thisCourse.homework);
-        newHomework.forEach(homework => {
+        const newHomework = await getAssignments(thisCourse.id, thisCourse.homework);
+        for (let j = 0; j < newHomework.length; j++) {
+            const homework = newHomework[j];
             // Tweet homework
             const homeworkStatus = '[' + thisCourse.name + '] ' + homework.title + ' (Due Date: ' + homework.due_date + ') ' + homework.link;
-            sendTwitter(homeworkStatus);
+            await sendTwitter(homeworkStatus);
             // Post homework in Discord
             const homeworkEmbed = new Discord.MessageEmbed()
                 .setColor('#4c84ed')
@@ -325,15 +327,16 @@ discord.setInterval(async () => {
                 .addField('Start Date:', homework.start_date)
                 .addField('Due Date:', homework.due_date)
                 .setTimestamp();
-            sendDiscord(homeworkEmbed, course.id);
-        });
+            await sendDiscord(homeworkEmbed, course.id);
+        }
 
         // Check for new tests
-        const newTests = getAssignments(thisCourse.id, thisCourse.tests);
-        newTests.forEach(test => {
+        const newTests = await getAssignments(thisCourse.id, thisCourse.tests);
+        for (let j = 0; j < newTests.length; j++) {
+            const test = newTests[j];
             // Tweet tests
             const testStatus = '[' + thisCourse.name + '] ' + test.title + ' (Date: ' + test.due_date + ') ' + test.link;
-            sendTwitter(testStatus);
+            await sendTwitter(testStatus);
             // Post tests in Discord
             const testEmbed = new Discord.MessageEmbed()
                 .setColor('#4eeb4b')
@@ -343,11 +346,11 @@ discord.setInterval(async () => {
                 .addField('Start Date:', test.start_date)
                 .addField('Due Date:', test.due_date)
                 .setTimestamp();
-            sendDiscord(testEmbed, course.id);
-        });
+            await sendDiscord(testEmbed, course.id);
+        }
 
         // Get recent mentions
-        const recentMentionsResponse = await fetch("https://api.twitter.com/2/tweets/search/recent?query=%40" + config.twitter.handle + "&tweet.fields=created_at,in_reply_to_user_id", twitterGetInit);
+        const recentMentionsResponse = await fetch("https://api.twitter.com/2/tweets/search/recent?query=%40" + config.twitter.handle + "&tweet.fields=created_at,in_reply_to_user_id&expansions=author_id", twitterGetInit);
         const recentMentionsObject = await recentMentionsResponse.json();
         const fiveMinutesAgo = new Date(Date.now() - 3e5);
         // Go through mentions and tweet replies
@@ -359,19 +362,19 @@ discord.setInterval(async () => {
             // This helps ignore retweets
             if (thisTweet.in_reply_to_user_id !== config.twitter.user_id) continue;
             // Determine which class is mentioned
-            let course = config.canvas.courses.find(c => thisTweet.search(new RegExp('(' + c.name + '|' + c.nick + ')', 'i')) > -1);
+            let course = config.canvas.courses.find(c => thisTweet.text.search(new RegExp('(' + c.name + '|' + c.nick + ')', 'i')) > -1);
             // Determine if we're looking for an announcement or assignment
             let objectToTweet;
-            if (thisTweet.search(/(announcement|news)/i) > -1) objectToTweet = announcements[course.id][0];
-            else if (thisTweet.search(/(homework|hw)/i) > -1) objectToTweet = assignments[course.id].homework.length === 0 ? {} : assignments[course.id].homework[0];
-            else if (thisTweet.search(/(test|exam|quiz|tests|exams|quizzes)/i) > -1) objectToTweet = assignments[course.id].tests.length === 0 ? {} : assignments[course.id].tests[0];
+            if (thisTweet.text.search(/(announcement|news)/i) > -1) objectToTweet = announcements[course.id][0];
+            else if (thisTweet.text.search(/(homework|hw)/i) > -1) objectToTweet = assignments[course.id].homework.length === 0 ? {} : assignments[course.id].homework[0];
+            else if (thisTweet.text.search(/(test|exam|quiz|tests|exams|quizzes)/i) > -1) objectToTweet = assignments[course.id].tests.length === 0 ? {} : assignments[course.id].tests[0];
             // If announcement or assignment is not mentioned, skip
             if (objectToTweet === undefined || objectToTweet === {}) continue;
             // Constructing the reply and tweeting
-            const date = thisTweet.search(/(homework|hw)/i) > -1 ? 'Due Date: ' + objectToTweet.due_date : thisTweet.search(/(announcement|news)/i) > -1 ? objectToTweet.date : 'Date: ' + objectToTweet.due_date;
+            const date = thisTweet.text.search(/(homework|hw)/i) > -1 ? 'Due Date: ' + objectToTweet.due_date : thisTweet.text.search(/(announcement|news)/i) > -1 ? objectToTweet.date : 'Date: ' + objectToTweet.due_date;
             const sender = recentMentionsObject.includes.users.find(u => u.id === thisTweet.author_id).username;
             const tweetContent = '@' + sender + ' ' + objectToTweet.title + ' (' + date + ') ' + objectToTweet.link;
-            sendTwitter(tweetContent, thisTweet.id);
+            await sendTwitter(tweetContent, thisTweet.id);
         }
     }
 }, 3e5);
